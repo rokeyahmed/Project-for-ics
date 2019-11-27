6d3aac7ec8e845759326b02cb6406d6db08f7dce
rokeyahmed <58241746+rokeyahmed@users.noreply.github.com>
2019-11-27 09:35:14 +0600

    Add files via upload

---
+++ ics_cpp.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i=2;
+    switch(i)
+    {
+    case 1:
+        printf("i am in case 1\n");
+    case 2:
+        printf("i am in case 2\n");
+    case 3:
+        printf("i am in case3\n");
+    default:
+        printf("i am in default\n");
+    }
+    return 0;
+    }
